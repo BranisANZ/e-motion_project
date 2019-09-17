5f29574d767f4d4b87f6a063679055db768d7e0e
AlexandreBalle <alexballe35@gmail.com>
2019-09-17 12:21:19 +0200

    update form vehicle

--- src/Form/RentalType.php
+++ src/Form/RentalType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Vehicle;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,13 +14,42 @@ class RentalType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('brand')
-            ->add('km')
-            ->add('matriculation')
-            ->add('year')
-            ->add('autonomie')
-            ->add('door')
-            ->add('place')
+            ->add('brand', TextType::class, [
+                'label' => 'Marque :',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])->add('km', IntegerType::class, [
+                'label' => 'Kilometrage :',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])->add('matriculation', TextType::class, [
+                'label' => 'Matriculation :',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])->add('year', TextType::class, [
+                'label' => 'Année d\'immatriculation :',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])->add('autonomie',IntegerType::class, [
+                'label' => 'Autonomie :',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])->add('door', IntegerType::class, [
+                'label' => 'Nombre de portes :',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])->add('place', IntegerType::class, [
+                'label' => 'Nombre de place :',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
         ;
     }
 
